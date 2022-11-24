feda7d5723a7ed21ba0cada187e5db36e6260eff
graveetone <zastavskijs@gmail.com>
2022-11-24 06:57:22 +0200

    mergen main into checker_on_frontend_side

--- frontend/src/components/Challenge.jsx
+++ frontend/src/components/Challenge.jsx
@@ -1,15 +1,78 @@
-import   React             from "react";
-import   Header            from "./Header";
-import { Link }            from "react-router-dom";
+import React, { useRef } from "react";
+import Header from "./Header";
+import { Link } from "react-router-dom";
 import { ArrowSmLeftIcon } from "@heroicons/react/solid";
-import   Editor            from "./helpers/Editor";
+import Editor from "./helpers/Editor";
+import checkCode from "../caw_requests";
+
+const ALL_PASSED_MSG = "All tests passed! You're genius!"
+const FAILURES_PRESENT_MSG = "There are some failures:( Fix them and try again!"
 
 const baseStyle = {
   border: '1px solid rgb(31, 41, 55)',
   backgroundColor: 'white'
 }
 
+const centerConsoleContentStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center'
+}
+const loading_src = 'loading.gif'
+
+function setLoadingCircle(consoleElement) {
+  consoleElement.innerHTML = `<img src=${loading_src}>`
+}
+
+function testView(test) {
+  return `<span style= "color: ${test.status === 'passed' ? 'green' : 'red'}" }}> ${test.description} </span><br>`
+}
+
+function errorView(error) {
+  return `<span style= "color: red" }}> ${error}</span><br>`
+}
+
+function displayResults(data, consoleElement) {
+  let html = 'Results:<br>'
+  let all_passed = true;
+
+  if (data.tests) {
+    for (let test of data.tests) {
+      html += testView(test)
+
+      if (test.status === 'failed') {
+        all_passed = false;
+      }
+
+      if (test.message) {
+        html += `   Error: ${test.message}<br>`
+      }
+    }
+
+    html += all_passed ? ALL_PASSED_MSG : FAILURES_PRESENT_MSG
+  }
+  else {
+    html += errorView(data.error)
+  }
+
+  consoleElement.innerHTML = html
+}
+
+async function sendCode(editorRef, consoleRef) {
+  setLoadingCircle(consoleRef.current)
+
+  let code = editorRef.current.editor.getValue()
+
+  let data = await (await checkCode(code, '3')).json();
+
+  displayResults(data, consoleRef.current);
+}
+
 function Challenge() {
+  const editorRef = useRef()
+  const consoleRef = useRef()
+
   return (
     <div>
       <Header />
@@ -21,8 +84,8 @@ function Challenge() {
           Complete code below to cleate a function {"'slugify'"} to generate
           unique url for article by it{"`"}s title
         </p>
-        <div className="bg-white rounded-2xl m-10 pl-5 mx-12 box-border flex flex-row-reverse sm:flex-col-reverse md:flex-col-reverse">
-          <div className="pt-12 pl-20 mr-20 w-3/4 flex flex-col justify-around self-center">
+        <div className="bg-white rounded-2xl m-10 pl-5 mx-12 box-border flex flex-row-reverse">
+          <div className="pt-12 pl-20 mr-24 w-full flex flex-col justify-between">
             <p className="font-semibold text-xl border-b-2 border-black inline-block mb-4">
               Examples
             </p>
@@ -49,7 +112,7 @@ function Challenge() {
                 <div className="bg-gray-100 rounded-full h-5 w-5 absolute left-16 top-5"></div>
               </div>
               <div className="rounded-3xl">
-                <Editor />
+                <Editor ref={editorRef} />
                 {/* br is used here as temporary fix for bug with bottom corners of editor container*/}
                 <br />
               </div>
--- frontend/src/components/helpers/Editor.jsx
+++ frontend/src/components/helpers/Editor.jsx
@@ -1,11 +1,12 @@
 import React     from 'react'
 import AceEditor from "react-ace";
-
-import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/src-noconflict/ext-language_tools";
 
-require(`ace-builds/src-noconflict/mode-python`);
-require(`ace-builds/src-noconflict/theme-github`);
+const language = 'python'
+const theme    = 'github'
+
+require(`ace-builds/src-noconflict/mode-${language}`);
+require(`ace-builds/src-noconflict/theme-${theme}`);
 
 const changeSize = (editorInstance) => {
   editorInstance.container.style.width = "100%";
@@ -17,23 +18,21 @@ const cawEditorOptions = {
   enableLiveAutocompletion: true,
   enableSnippets: false,
   showLineNumbers: true,
-  tabSize: 2,
-};
-
-export default function Editor() {
-  return (
-    <AceEditor
-      onLoad={changeSize}
-      placeholder="# Put your code here! Good luck!"
-      mode="python"
-      theme="github"
-      name="cawEditor"
-      fontSize={18}
-      showPrintMargin={true}
-      showGutter={true}
-      highlightActiveLine={true}
-      value={""}
-      setOptions={cawEditorOptions}
-    />
-  );
+  tabSize: 2
 }
+export default Editor = React.forwardRef((props, ref) => (
+    <AceEditor
+    onLoad              = { changeSize }
+    placeholder         = "# Put your code here! Good luck!"
+    mode                = { language }
+    theme               = { theme }
+    name                = "cawEditor"
+    fontSize            = { 18 }
+    showPrintMargin     = { true }
+    showGutter          = { true }
+    highlightActiveLine = { true }
+    value               = { '' }
+    setOptions          = { cawEditorOptions }
+    ref                 = { ref } />
+  )
+);
